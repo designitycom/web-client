efefcf52cc8fde9ec6ade78cd08ccd3bbd8ccf6f
AmirSaber Sharifi <inbox@saberistic.com>
2023-05-29 11:37:31 +0000

    added service worker,env,collection endpoints

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import Home from './pages/home';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,6 +12,9 @@ root.render(
     <Home />
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
